8f125a7d05e7d27b51b8600a030f1f0fbaa64678
carloscordova.dev <cordovamanuel22@icloud.com>
2024-03-03 14:55:03 -0800

    feat: :construction: continue botton done
    ocupo agredar funcionalidad a los botones

--- index.js
+++ index.js
@@ -1,3 +1,47 @@
-const btnOne = document.querySelector('.btn-1').value
-const display = document.querySelector('.display').value
-console.log(display);
+//! ----==== SELECTORS ===-------
+const btnCero = document.querySelector('.btn-0').value;
+const btnOne = document.querySelector('.btn-1').value;
+const btnTwo = document.querySelector('.btn-2').value;
+const btnThre = document.querySelector('.btn-3').value;
+const btnFour = document.querySelector('.btn-4').value;
+const btnFive = document.querySelector('.btn-5').value;
+const btnSix = document.querySelector('.btn-6').value;
+const btnSevent = document.querySelector('.btn-7').value;
+const btnEight = document.querySelector('.btn-8').value;
+const btnNine = document.querySelector('.btn-9').value;
+const btnResta = document.querySelector('.btn-resta ').value;
+const btnSuma = document.querySelector('.btn-suma').value;
+const accountNumber = document.querySelector('.accountNumber');
+const form = document.querySelector('.form'); // selects from
+const btnRetiro = document.querySelector('.btn-retiro');
+const btnDeposito = document.querySelector('.btn-deposito');
+const btnContinuar = document.querySelector('.btn-continuar');
+const btnCancelar = document.querySelector('.btn-cancelar');
+const screen = document.querySelector('.screen');
+//! ------===EVENT LISTENER ===-----
+window.addEventListener('load', check)
+
+function check() {
+  btnContinuar.addEventListener('click', handleAcountNum)
+
+}
+
+function handleAcountNum(e) {
+  e.preventDefault()
+  if (accountNumber.value == 1234) {
+      screen.innerHTML = "" 
+      screen.innerHTML +=`
+      <h1>User Found</h1>
+      <h2>Selecccion una opcion : Retiro o Deposito</h2>
+      ` 
+
+
+    
+  }else {
+    screen.innerHTML = ""
+    screen.innerHTML += `
+      <h1>User Found not found </h1>
+      <h2>Verificar numero de cuenta e inter de nuevo </h2>
+      ` 
+  }
+}
